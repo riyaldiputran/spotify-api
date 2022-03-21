b070ec0753021c36a737e4c14526834beafb21f7
riyaldiputran <riyaldiputran007@gmail.com>
2022-03-21 19:13:38 +0800

    Update App.js menambahkan gambar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const baik = "baik";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+
+
+        <input type="text" name="textfield" placeholder="Search"></input>
+
+        <button type="button">Search</button>
+        <img src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif
+        " alt="Italian Trulli"></img>
+
+        {baik === "baik" ? <p> react ternyata baik</p> : <p> react ternyata jahat</p>}
       </header>
     </div>
   );
